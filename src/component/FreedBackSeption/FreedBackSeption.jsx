0486e3fa0e8be4dbf06262b9529895420f1e7ec7
kanuni <mister.nicto006@gmail.com>
2024-01-23 22:29:40 +0300

    use

--- src/component/FreedBackSeption/FreedBackSeption.jsx
+++ src/component/FreedBackSeption/FreedBackSeption.jsx
@@ -1,52 +1,85 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./FreedBackSeption.css";
 import Button from "../Button/Button";
 
+const StatevsRef = () => {
+  const input = useRef();
+  const [show, setShow] = useState(false);
+
+
+  console.log(input);
+  
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setShow(true);
+    }
+  };
+
+  return (
+    <div>
+      <h3>input value: {show && input.current.value}</h3>
+      <input
+        type="text"
+        ref={input}
+        onKeyDown={handleKeyDown}
+        className="control"
+      />
+    </div>
+  );
+};
+
 const FreedBackSeption = () => {
-  const [name, setName] = useState("");
-  const [hasError, setHasError] = useState(true);
-  const [reason, setReason] = useState("help");
+  const [form, setForm] = useState({
+    name: "",
+    hasError: false,
+    reason: "help",
+  });
 
   const handleNameChange = (e) => {
-    console.log(e.target.value);
-
-    setName(e.target.value);
-    setHasError(e.target.value.trim().length === 0)
+    setForm((prev) => ({
+      ...prev,
+      name: e.target.value,
+      hasError: e.target.value.trim().length === 0,
+    }));
   };
+
   return (
     <section>
       <h3>Обратная связь</h3>
 
-      <form>
+      <form style={{ marginBottom: "1rem" }}>
         <label htmlFor="name">Ваше Имя</label>
         <input
           type="text"
           id="name"
           className="control"
-          value={name}
+          value={form.name}
           style={{
-            border:hasError ? '1px solid red': null,
+            border: form.hasError ? "1px solid red" : null,
           }}
           onChange={handleNameChange}
         />
         <label htmlFor="reason">Причина обращения</label>
-        <select id="reason" className="control" value={reason} onChange={ (e ) => setReason(e.target.value)}>
+        <select
+          id="reason"
+          className="control"
+          value={form.reason}
+          onChange={(e) =>
+            setForm((prev) => ({ ...prev, reason: e.target.value }))
+          }
+        >
           <option value="error">Ошибка</option>
           <option value="help">Нужна помощь</option>
           <option value="suggest">Предложение</option>
         </select>
 
-        <pre>
-          Name: {name}
-          <br />
-          Reason: {reason}
-        </pre>
-
-
-        <Button disabled={hasError} 
-        isActive={!hasError}
-        >Отправить</Button>
+        <Button disabled={form.hasError} isActive={!form.hasError}>
+          Отправить
+        </Button>
+        <hr />
       </form>
+
+      <StatevsRef />
     </section>
   );
 };
